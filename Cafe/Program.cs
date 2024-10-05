1b6d8ae6d31fbf5ec5cfa2f6d6456624f0dee997
Sobh6n <sobhankhedry7297@gmail.com>
2024-10-05 12:12:08 +0330

    updated

--- Cafe/Program.cs
+++ Cafe/Program.cs
@@ -9,154 +9,186 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             string filePath = @"C:\Users\sobha\OneDrive\Desktop\Sobhan\University\term 5\Database\Customers.txt";
-            Console.WriteLine("0_Login \n" +
-                              "1_SignUp  \n");
-            var first = Int32.Parse(Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("0_Login \n" +
+                                  "1_SignUp  \n");
+                var first = Int32.Parse(Console.ReadLine());
 
 
-            if (first == 1)
-            {
-                string[] existingCustomers = File.ReadAllLines(filePath);
-                int customerCount = existingCustomers.Length;
-                StreamWriter sw = File.AppendText(filePath);
-                Customer customer = new Customer();
-                customer.ID = customerCount++;
-                sw.Write(" ID : " + customer.ID);
-                customer.Role = "User";
-                sw.Write(", Role : " + customer.Role);
-                Console.WriteLine(" enter you Username : \n");
-                string fullName = Console.ReadLine();
-                customer.userName = fullName;
-                sw.Write(", Username : " + fullName);
-                Console.WriteLine(" enter you Password : \n");
-                string Password = Console.ReadLine();
-                customer.Password = Password;
-                sw.Write($", Password : {Password} ");
-                Console.WriteLine(" What is your Email Address? \n");
-                string email = Console.ReadLine();
-                sw.Write(", Email : " + email);
-                sw.Write("\n");
-                sw.Close();
-            }
-            int val = 0;
-            string b = "";
-            Customer cus = new Customer();
-            if (first == 0)
-            {
-                Console.WriteLine("enter your username : ");
-                string username = Console.ReadLine();
-                Console.WriteLine("enter your password : ");
-                string password = Console.ReadLine();
-                string[] gettingCustomers = File.ReadAllLines(filePath);
-                string[] customers = new string[gettingCustomers.Length];
-                int flag = 0;
-                int blag = 0;
-                string[] word = new string[100];
-                foreach (string customer in gettingCustomers)
+                if (first == 1)
                 {
-                    if ((customer.Contains($"Username : {username}")) && (customer.Contains($"Password : {password}")) &&
-                        (customer.Contains($"Role : Admin")))
+                    string[] existingCustomers = File.ReadAllLines(filePath);
+                    int customerCount = existingCustomers.Length;
+                    StreamWriter sw = File.AppendText(filePath);
+                    Customer customer = new Customer();
+                    customer.ID = customerCount++;
+                    sw.Write(" ID : " + customer.ID);
+                    customer.Role = "User";
+                    sw.Write(", Role : " + customer.Role);
+                    Console.WriteLine(" enter you Username : \n");
+                    string fullName = Console.ReadLine();
+                    customer.userName = fullName;
+                    sw.Write(", Username : " + fullName);
+                    Console.WriteLine(" enter you Password : \n");
+                    string Password = Console.ReadLine();
+                    customer.Password = Password;
+                    sw.Write($", Password : {Password} ");
+                    Console.WriteLine(" What is your Email Address? \n");
+                    string email = Console.ReadLine();
+                    sw.Write(", Email : " + email);
+                    sw.Write("\n");
+                    sw.Close();
+                }
+                int val = 0;
+                string b = "";
+                Customer cus = new Customer();
+                if (first == 0)
+                {
+                    Console.WriteLine("enter your username : ");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("enter your password : ");
+                    string password = Console.ReadLine();
+                    string[] gettingCustomers = File.ReadAllLines(filePath);
+                    string[] customers = new string[gettingCustomers.Length];
+                    int flag = 0;
+                    int blag = 0;
+                    string[] word = new string[100];
+                    foreach (string customer in gettingCustomers)
                     {
-                        word = customer.Split(',');
-
-                        //extracting id
-                        string findingId = word[0];
-                        for (int i = 0; i < findingId.Length; i++)
+                        if ((customer.Contains($"Username : {username}")) && (customer.Contains($"Password : {password}")) &&
+                            (customer.Contains($"Role : Admin")))
                         {
-                            if (Char.IsDigit(findingId[i]))
-                                b += findingId[i];
+                            word = customer.Split(',');
+
+                            //extracting id
+                            string findingId = word[0];
+                            for (int i = 0; i < findingId.Length; i++)
+                            {
+                                if (Char.IsDigit(findingId[i]))
+                                    b += findingId[i];
+                            }
+
+                            if (b.Length > 0)
+                            {
+                                val = int.Parse(b);
+
+                                cus.ID = val;
+                            }
+
+
+                            //extractin Username :
+                            string findingUsernamew = word[2];
+                            string[] extractuserName = findingUsernamew.Split(" ");
+                            cus.userName = extractuserName[3];
+
+
+                            //etracting password
+                            string findPassword = word[3];
+                            string[] extractPass = findPassword.Split(" ");
+                            cus.Password = extractPass[3];
+
+
+                            //extracting email
+                            string finemailw = word[4];
+                            string[] extractEmail = finemailw.Split(" ");
+                            cus.Mail = extractEmail[3];
+
+
+                            flag = 1;
+                            blag = 1;
+                            break;
+
                         }
 
-                        if (b.Length > 0)
+                        if ((customer.Contains($"Username : {username}")) && (customer.Contains($"Password : {password}")))
                         {
-                            val = int.Parse(b);
+                            word = customer.Split(',');
 
-                            cus.ID = val;
+                            // extractinID
+                            string findingId = word[0];
+                            for (int i = 0; i < findingId.Length; i++)
+                            {
+                                if (Char.IsDigit(findingId[i]))
+                                    b += findingId[i];
+                            }
+
+                            if (b.Length > 0)
+                            {
+                                val = int.Parse(b);
+
+                                cus.ID = val;
+                            }
+
+                            //extractin Username :
+                            string findingUsernamew = word[2];
+                            string[] extractuserName = findingUsernamew.Split(" ");
+                            cus.userName = extractuserName[3];
+
+                            //etracting password
+                            string findPassword = word[3];
+                            string[] extractPass = findPassword.Split(" ");
+                            cus.Password = extractPass[3];
+
+                            //extracting email
+                            string finemailw = word[4];
+                            string[] extractEmail = finemailw.Split(" ");
+                            cus.Mail = extractEmail[3];
+
+
+                            flag = 1;
+                            break;
+
                         }
-
-
-                        //extractin Username :
-                        string findingUsernamew = word[2];
-                        string[] extractuserName = findingUsernamew.Split(" ");
-                        cus.userName = extractuserName[3];
-
-
-                        //etracting password
-                        string findPassword = word[3];
-                        string[] extractPass = findPassword.Split(" ");
-                        cus.Password = extractPass[3];
-
-
-                        //extracting email
-                        string finemailw = word[4];
-                        string[] extractEmail = finemailw.Split(" ");
-                        cus.Mail = extractEmail[3];
-
-
-                        flag = 1;
-                        blag = 1;
-                        break;
 
                     }
 
-                    if ((customer.Contains($"Username : {username}")) && (customer.Contains($"Password : {password}")))
+                    if ((flag == 1) && (blag == 1))
                     {
-                        word = customer.Split(',');
-
-                        // extractinID
-                        string findingId = word[0];
-                        for (int i = 0; i < findingId.Length; i++)
+                        while (true)
                         {
-                            if (Char.IsDigit(findingId[i]))
-                                b += findingId[i];
+                            Console.WriteLine("what do you want to do?");
+                            Console.WriteLine("1_Insert Item");
+                            Console.WriteLine("2_Delete Item");
+                            Console.WriteLine("3_Buy Ingrediants");
+                            Console.WriteLine("4_Net(Sum of income and outcome)");
+                            int Do = Int32.Parse(Console.ReadLine());
+                            if (Do == 1)
+                            {
+
+                            }
+                            if (Do == 2)
+                            {
+
+                            }
+                            if (Do == 3)
+                            {
+
+                            }
+                            if (Do == 4)
+                            {
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Something went wrong try again!!");
+                            }
                         }
-
-                        if (b.Length > 0)
-                        {
-                            val = int.Parse(b);
-
-                            cus.ID = val;
-                        }
-
-                        //extractin Username :
-                        string findingUsernamew = word[2];
-                        string[] extractuserName = findingUsernamew.Split(" ");
-                        cus.userName = extractuserName[3];
-
-                        //etracting password
-                        string findPassword = word[3];
-                        string[] extractPass = findPassword.Split(" ");
-                        cus.Password = extractPass[3];
-
-                        //extracting email
-                        string finemailw = word[4];
-                        string[] extractEmail = finemailw.Split(" ");
-                        cus.Mail = extractEmail[3];
-
-
-                        flag = 1;
+                    }
+                    if (flag == 1 && blag == 0)
+                    {
+                        Console.WriteLine($"welcome user your id is {cus.ID} with {cus.userName} username");
+                        Console.WriteLine($" with {cus.Password} password  {cus.Mail} email");
                         break;
+                    }
+                    if (flag == 0)
+                    {
+                        Console.WriteLine("USername or password is wrong \n");
 
                     }
 
-                }
 
-                if ((flag == 1) && (blag == 1))
-                {
-                    Console.WriteLine($"hello admin your ID is {cus.ID}");
-                    Console.WriteLine($" with {cus.Password} password and  {cus.Mail} email");
                 }
-                if (flag == 1 && blag == 0)
-                {
-                    Console.WriteLine($"Hello user your id is {cus.ID} with {cus.userName} username");
-                    Console.WriteLine($" with {cus.Password} password  {cus.Mail} email");
-                }
-                if (flag == 0)
-                {
-                    Console.WriteLine("USername or password is wrong");
-                }
-
-
             }
         }
     }
