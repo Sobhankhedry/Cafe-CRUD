8e77d777c9e19cbc62ef9e7be7ca02679b302159
Sobh6n <sobhankhedry7297@gmail.com>
2024-10-05 10:44:47 +0330

    ca extract ID

--- Cafe/Program.cs
+++ Cafe/Program.cs
@@ -9,8 +9,10 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             string filePath = @"C:\Users\sobha\OneDrive\Desktop\Sobhan\University\term 5\Data Base\Customers.txt";
-            Console.WriteLine("Login or SignUp : ");
+            Console.WriteLine("0_Login \n" +
+                              "1_SignUp  \n");
             var first = Int32.Parse(Console.ReadLine());
+
 
             if (first == 1)
             {
@@ -36,10 +38,86 @@ namespace HelloWorldApp
                 sw.Write("\n");
                 sw.Close();
             }
+            int val = 0;
+            string b = "";
+            Customer cus = new Customer();
             if (first == 0)
             {
-                string[] existingCustomers = File.ReadAllLines(filePath);
-                Console.WriteLine(existingCustomers[1]);
+                Console.WriteLine("enter your username : ");
+                string username = Console.ReadLine();
+                Console.WriteLine("enter your password : ");
+                string password = Console.ReadLine();
+                string[] gettingCustomers = File.ReadAllLines(filePath);
+                string[] customers = new string[gettingCustomers.Length];
+                int flag = 0;
+                int blag = 0;
+                string[] word = new string[100];
+                foreach (string customer in gettingCustomers)
+                {
+                    if ((customer.Contains($"Username : {username}")) && (customer.Contains($"Password : {password}")) &&
+                        (customer.Contains($"Role : Admin")))
+                    {
+                        word = customer.Split(',');
+
+                        //extracting id
+                        string findingId = word[0];
+                        for (int i = 0; i < findingId.Length; i++)
+                        {
+                            if (Char.IsDigit(findingId[i]))
+                                b += findingId[i];
+                        }
+
+                        if (b.Length > 0)
+                        {
+                            val = int.Parse(b);
+
+                            cus.ID = val;
+                        }
+
+                        flag = 1;
+                        blag = 1;
+                        break;
+
+                    }
+
+                    if ((customer.Contains($"Username : {username}")) && (customer.Contains($"Password : {password}")))
+                    {
+                        string findingId = word[0];
+                        for (int i = 0; i < findingId.Length; i++)
+                        {
+                            if (Char.IsDigit(findingId[i]))
+                                b += findingId[i];
+                        }
+
+                        if (b.Length > 0)
+                        {
+                            val = int.Parse(b);
+
+                            cus.ID = val;
+                        }
+
+
+                        flag = 1;
+                        break;
+
+                    }
+
+                }
+
+                if ((flag == 1) && (blag == 1))
+                {
+                    Console.WriteLine($"hello admin your ID is {cus.ID}");
+                }
+                if (flag == 1 && blag == 0)
+                {
+                    Console.WriteLine($"Hello user your id is {cus.ID}");
+                }
+                if (flag == 0)
+                {
+                    Console.WriteLine("USername or password is wrong");
+                }
+
+
             }
         }
     }
